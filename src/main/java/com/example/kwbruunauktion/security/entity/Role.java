a717cab9b4955c460dfda1c883ec4e3323b92347
Mojens <moha135h@stud.kea.dk>
2022-11-21 15:58:16 +0100

    Update roles

--- src/main/java/com/example/kwbruunauktion/security/entity/Role.java
+++ src/main/java/com/example/kwbruunauktion/security/entity/Role.java
@@ -4,6 +4,7 @@ package com.example.kwbruunauktion.security.entity;
 Add required roles for your project to this enum
  */
 public enum Role {
-    USER,
-    ADMIN
+    BUYER,
+    ADMIN,
+    ECONOMY
 }
