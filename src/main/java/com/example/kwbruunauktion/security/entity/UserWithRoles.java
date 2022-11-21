16a8503023dba7af7e3b13ae97a48a137a63e2be
Mojens <moha135h@stud.kea.dk>
2022-11-21 16:08:03 +0100

    Add new role

--- src/main/java/com/example/kwbruunauktion/security/entity/UserWithRoles.java
+++ src/main/java/com/example/kwbruunauktion/security/entity/UserWithRoles.java
@@ -54,7 +54,7 @@ public class UserWithRoles implements UserDetails {
     private LocalDateTime edited;
 
     @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "ENUM('USER','ADMIN')")
+    @Column(columnDefinition = "ENUM('BUYER','ADMIN','ECONOMY')")
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "security_role")
     List<Role> roles = new ArrayList<>();
