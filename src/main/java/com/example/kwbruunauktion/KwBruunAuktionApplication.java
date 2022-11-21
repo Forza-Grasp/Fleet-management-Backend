79859a7909750e7f27a3bc30c75a855b81d8b0c1
Mojens <moha135h@stud.kea.dk>
2022-11-21 09:57:32 +0100

    first commit

---
+++ src/main/java/com/example/kwbruunauktion/KwBruunAuktionApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwbruunauktion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwBruunAuktionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KwBruunAuktionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/kwbruunauktion/KwBruunAuktionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwbruunauktion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwBruunAuktionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
