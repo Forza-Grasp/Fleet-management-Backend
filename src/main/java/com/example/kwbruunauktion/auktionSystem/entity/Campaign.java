76080f5213e40d9681fb7e008c27a473cd5c6970
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-11-24 14:06:18 +0100

    Campaign Entitiy CampaignCar Entity

--- src/main/java/com/example/kwbruunauktion/auktionSystem/entity/Campaign.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/entity/Campaign.java
@@ -1,8 +1,12 @@
 package com.example.kwbruunauktion.auktionSystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -19,4 +23,15 @@ public class Campaign {
 
     @OneToOne (cascade = CascadeType.ALL)
     private CampaignCar campaignCar;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 100)
+    private CampaignStatus campaignStatus;
+
+    @Column(nullable = false, length = 100)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate activeDate;
+
+    @OneToMany (mappedBy = "campaign")
+    private List<LcdvCodes> lcdvCodes = new ArrayList<>();
 }
