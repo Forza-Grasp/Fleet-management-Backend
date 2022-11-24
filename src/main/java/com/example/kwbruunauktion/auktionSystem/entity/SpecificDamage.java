ee716f9eae8f487ab2358b84d46a3b29cb708319
ChineseThunder <simo09j9@stud.kea.dk>
2022-11-24 13:44:53 +0100

    entity

---
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/entity/SpecificDamage.java
@@ -0,0 +1,27 @@
+package com.example.kwbruunauktion.auktionSystem.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+
+@Entity
+public class SpecificDamage {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 450,nullable = false)
+    private String damages;
+
+    @Column(length = 50,nullable = false)
+    private double price;
+
+    @ManyToOne
+    DamageMatrix damageMatrix;
+}
