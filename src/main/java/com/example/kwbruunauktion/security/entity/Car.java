ed1dde297098d74eef352c9ecac95a6a120c29f2
MBHNielsenX <mads.nielsen12@live.dk>
2022-11-24 12:52:46 +0100

    Tester

---
+++ src/main/java/com/example/kwbruunauktion/security/entity/Car.java
@@ -0,0 +1,10 @@
+package com.example.kwbruunauktion.security.entity;
+
+import java.time.LocalDate;
+
+public class Car {
+    long id;
+    String chassisNumber;
+    String facModelCodeL;
+    LocalDate firstRegDate;
+}
