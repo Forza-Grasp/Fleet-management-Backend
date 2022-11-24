27e3e8ef9cbff0e2fe6ce419a91dbed7ab394c30
MBHNielsenX <mads.nielsen12@live.dk>
2022-11-24 13:56:30 +0100

    long has been changed to Long

--- src/main/java/com/example/kwbruunauktion/security/entity/SpecificCar.java
+++ src/main/java/com/example/kwbruunauktion/security/entity/SpecificCar.java
@@ -18,7 +18,7 @@ public class SpecificCar {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(length = 30, nullable = false, unique = true)
     private String chassisNumber;
