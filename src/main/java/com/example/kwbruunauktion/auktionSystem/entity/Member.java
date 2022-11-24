b23fb6decc37ebd4760856d154bd0c0725ff95b5
Mojens <moha135h@stud.kea.dk>
2022-11-24 12:50:42 +0100

    Remove test

---
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/entity/Member.java
@@ -0,0 +1,49 @@
+package com.example.kwbruunauktion.auktionSystem.entity;
+
+import com.example.kwbruunauktion.security.entity.UserWithRoles;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Member extends UserWithRoles {
+
+  @Column(length = 30, nullable = false)
+  private String firstName;
+
+  @Column(length = 30, nullable = false)
+  private String lastName;
+
+  @Column(length = 30, nullable = false)
+  private String phoneNumber;
+
+  @Column(length = 30, nullable = false)
+  private String companyName;
+
+  @Column(length = 30, nullable = false)
+  private String companyEuVatNumber;
+
+  @Column(length = 30, nullable = false)
+  private String addressLine1;
+
+  @Column(length = 30)
+  private String addressLine2;
+
+  @Column(length = 30, nullable = false)
+  private String city;
+
+  @Column(length = 30, nullable = false)
+  private String zipCode;
+
+  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+  List<SpecificCarModel> ViewableCarBrands;
+
+}
