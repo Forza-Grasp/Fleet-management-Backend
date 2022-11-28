c3c3ec6a415262ae0541dc6ecbd16bac413fb53c
Mojens <91475420+Mojens@users.noreply.github.com>
2022-11-28 13:11:49 +0100

    Update UserBuyerRepository.java

--- src/main/java/com/example/kwbruunauktion/auktionSystem/repository/UserBuyerRepository.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/repository/UserBuyerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserBuyerRepository extends JpaRepository<UserBuyer, String> {
+public interface UserBuyerRepository extends JpaRepository<UserBuyer, Long> {
 
 }
