c5124e1df36f9f85bba4b9095b965fbfbabad4b8
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-11-28 16:21:59 +0100

    Added UserBuyerRequest

--- src/main/java/com/example/kwbruunauktion/auktionSystem/service/users/UserBuyerService.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/service/users/UserBuyerService.java
@@ -1,7 +1,18 @@
 package com.example.kwbruunauktion.auktionSystem.service.users;
 
+import com.example.kwbruunauktion.auktionSystem.repository.UserBuyerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserBuyerService {
+
+    UserBuyerRepository userBuyerRepository;
+
+    public UserBuyerService(UserBuyerRepository userBuyerRepository) {
+        this.userBuyerRepository = userBuyerRepository;
+    }
+
+    // View all users
+
+
 }
