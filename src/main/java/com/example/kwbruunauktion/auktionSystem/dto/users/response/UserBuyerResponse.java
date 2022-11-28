8c6af9ea565940014a01415cee629b7e8e11963c
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-11-28 16:22:52 +0100

    Added UserBuyerRequest

--- src/main/java/com/example/kwbruunauktion/auktionSystem/dto/users/response/UserBuyerResponse.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/dto/users/response/UserBuyerResponse.java
@@ -1,4 +1,15 @@
 package com.example.kwbruunauktion.auktionSystem.dto.users.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserBuyerResponse {
 }
