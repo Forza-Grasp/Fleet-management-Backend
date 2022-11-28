46247359fb826b0df9eccf9112785b088e5540b7
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-11-28 13:18:39 +0100

    added entity attributes to UserAdmin.java

--- src/main/java/com/example/kwbruunauktion/auktionSystem/entity/UserAdmin.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/entity/UserAdmin.java
@@ -1,4 +1,31 @@
 package com.example.kwbruunauktion.auktionSystem.entity;
 
-public class UserAdmin {
+import com.example.kwbruunauktion.security.entity.UserWithRoles;
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class UserAdmin extends UserWithRoles {
+    @Column(length = 100, nullable = false)
+    private String firstName;
+
+    @Column(length = 100, nullable = false)
+    private String lastName;
+
+    @Column(length = 100, nullable = false)
+    private String phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Ownership ownership;
 }
