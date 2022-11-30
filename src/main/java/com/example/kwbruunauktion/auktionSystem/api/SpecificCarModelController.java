22d03fa0b29ace27ad57a929b24b87f67613a6a3
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-11-30 13:02:36 +0100

    removed endpoint specification "/all" from SpecificCarModelController

--- src/main/java/com/example/kwbruunauktion/auktionSystem/api/SpecificCarModelController.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/api/SpecificCarModelController.java
@@ -24,7 +24,7 @@ public class SpecificCarModelController {
         return specificCarModelService.getSpecificCarModelById(id);
     }
 
-    @GetMapping("/all")
+    @GetMapping()
     List<SpecificCarModelResponse> getAllSpecificCarModels() {return specificCarModelService.getSpecificCarModels();}
 
 
