f612a2e77b831564136c5a989051845ecd8d3af8
Mojens <moha135h@stud.kea.dk>
2022-12-05 19:25:38 +0100

    Har fået det til at virke med application runner

--- src/main/java/com/example/kwbruunauktion/auktionSystem/configuration/CampaignSetup.java
+++ src/main/java/com/example/kwbruunauktion/auktionSystem/configuration/CampaignSetup.java
@@ -7,6 +7,7 @@ import com.example.kwbruunauktion.auktionSystem.enums.CampaignStatus;
 import com.example.kwbruunauktion.auktionSystem.repository.CampaignRepository;
 import com.example.kwbruunauktion.auktionSystem.repository.LcdvCodesRepository;
 import com.example.kwbruunauktion.auktionSystem.service.CampaignService;
+import lombok.SneakyThrows;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
@@ -17,22 +18,42 @@ import java.util.List;
 
 @Controller
 public class CampaignSetup implements ApplicationRunner {
-        CampaignRepository campaignRepository;
-        CampaignService campaignService;
-        LcdvCodesRepository lcdvCodesRepository;
-
+    CampaignRepository campaignRepository;
+    CampaignService campaignService;
+    LcdvCodesRepository lcdvCodesRepository;
 
 
     public CampaignSetup(CampaignRepository campaignRepository, CampaignService campaignService, LcdvCodesRepository lcdvCodesRepository) {
-            this.campaignRepository = campaignRepository;
-            this.campaignService = campaignService;
-            this.lcdvCodesRepository = lcdvCodesRepository;
-        }
+        this.campaignRepository = campaignRepository;
+        this.campaignService = campaignService;
+        this.lcdvCodesRepository = lcdvCodesRepository;
+    }
 
     @Override
-        public void run(ApplicationArguments args) throws Exception {
+    @SneakyThrows
+    public void run(ApplicationArguments args) {
 
 
+        List<LcdvCodes> lcdvCodes = List.of(
+                LcdvCodes.builder()
+                        .lcdvCode("0")
+                        .build(),
+                LcdvCodes.builder()
+                        .lcdvCode("1")
+                        .build(),
+                LcdvCodes.builder()
+                        .lcdvCode("2")
+                        .build(),
+                LcdvCodes.builder()
+                        .lcdvCode("3")
+                        .build(),
+                LcdvCodes.builder()
+                        .lcdvCode("4")
+                        .build(),
+                LcdvCodes.builder()
+                        .lcdvCode("5")
+                        .build()
+        );
         Campaign campaign1 = Campaign.builder()
                 .campaignCar(CampaignCar.builder()
                         .brand("Audi")
@@ -51,37 +72,11 @@ public class CampaignSetup implements ApplicationRunner {
                         .latestExceptedReturnDate(LocalDate.of(2021, 10, 10))
                         .build())
                 .campaignStatus(CampaignStatus.ACTIVE)
+                .lcdvCodes(lcdvCodes)
                 .activeDate(LocalDate.now())
                 .build();
-
-        List<LcdvCodes> lcdvCodes = List.of(
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("0")
-                        .build(),
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("1")
-                        .build(),
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("2")
-                        .build(),
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("3")
-                        .build(),
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("4")
-                        .build(),
-                LcdvCodes.builder()
-                        .campaign(Collections.singletonList(campaign1))
-                        .lcdvCode("5")
-                        .build()
-        );
         campaignRepository.save(campaign1);
         lcdvCodesRepository.saveAll(lcdvCodes);
 
-        }
+    }
 }
